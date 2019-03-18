63c11c51652bccecdb837113c085d384ead2d6bb
Erik Bergstrom <ebergstr@Eriks-MacBook-Pro.local>
2019-03-18 15:03:50 -0700

    Changed to chromosome-based input vcf files

--- SigProfilerMatrixGenerator/scripts/convert_input_to_simple_files.py
+++ SigProfilerMatrixGenerator/scripts/convert_input_to_simple_files.py
@@ -40,6 +40,7 @@ def convertVCF (project, vcf_path, genome, output_path, ncbi_chrom, log_file):
 	first_incorrect_file = True
 	out = open(log_file, 'a')
 	prev_line = None
+	first_chrom = ''
 	
 	# Iterates through each file 
 	for file in files:
@@ -102,14 +103,48 @@ def convertVCF (project, vcf_path, genome, output_path, ncbi_chrom, log_file):
 							continue
 
 						if first_SNV:
-							outputFile = output_path + "SNV/" + project + ".genome"
-							os.system("rm -f " + outputFile)
 							if not os.path.exists(output_path + "SNV/"):
 								os.mkdir(output_path + 'SNV/')
-							out_snv = open(outputFile, "w") 
+							outputFile_X = open(output_path + "SNV/X_" + project + ".genome", "w")
+							outputFile_Y = open(output_path + "SNV/Y_" + project + ".genome", "w")
+							outputFile_1 = open(output_path + "SNV/1_" + project + ".genome", "w")
+							outputFile_2 = open(output_path + "SNV/2_" + project + ".genome", "w")
+							outputFile_3 = open(output_path + "SNV/3_" + project + ".genome", "w")
+							outputFile_4 = open(output_path + "SNV/4_" + project + ".genome", "w")
+							outputFile_5 = open(output_path + "SNV/5_" + project + ".genome", "w")
+							outputFile_6 = open(output_path + "SNV/6_" + project + ".genome", "w")
+							outputFile_7 = open(output_path + "SNV/7_" + project + ".genome", "w")
+							outputFile_8 = open(output_path + "SNV/8_" + project + ".genome", "w")
+							outputFile_9 = open(output_path + "SNV/9_" + project + ".genome", "w")
+							outputFile_10 = open(output_path + "SNV/10_" + project + ".genome", "w")
+							outputFile_11 = open(output_path + "SNV/11_" + project + ".genome", "w")
+							outputFile_12 = open(output_path + "SNV/12_" + project + ".genome", "w")
+							outputFile_13 = open(output_path + "SNV/13_" + project + ".genome", "w")
+							outputFile_14 = open(output_path + "SNV/14_" + project + ".genome", "w")
+							outputFile_15 = open(output_path + "SNV/15_" + project + ".genome", "w")
+							outputFile_16 = open(output_path + "SNV/16_" + project + ".genome", "w")
+							outputFile_17 = open(output_path + "SNV/17_" + project + ".genome", "w")
+							outputFile_18 = open(output_path + "SNV/18_" + project + ".genome", "w")
+							outputFile_19 = open(output_path + "SNV/19_" + project + ".genome", "w")
+							outputFile_20 = open(output_path + "SNV/20_" + project + ".genome", "w")
+							outputFile_21 = open(output_path + "SNV/21_" + project + ".genome", "w")
+							outputFile_22 = open(output_path + "SNV/22_" + project + ".genome", "w")
+
+							outFiles = {'X': outputFile_X, 'Y':outputFile_Y, '1':outputFile_1, '2':outputFile_2, '3':outputFile_3, '4':outputFile_4,
+										'5':outputFile_5, '6':outputFile_6, '7':outputFile_7, '8':outputFile_8, '9':outputFile_9, '10':outputFile_10,
+										'11':outputFile_11, '12':outputFile_12, '13':outputFile_13, '14':outputFile_14, '15':outputFile_15, '16':outputFile_16,
+										'17':outputFile_17, '18':outputFile_18, '19':outputFile_19, '20':outputFile_20, '21':outputFile_21, '22':outputFile_22}
+							#out_snv = open(outputFile, "w") 
 							first_SNV = False
 
-						print("\t".join([project, sample, ".", genome, "SNV", chrom, start, start, ref, mut, "SOMATIC"]), file=out_snv)
+						#print("\t".join([project, sample, ".", genome, "SNV", chrom, start, start, ref, mut, "SOMATIC"]), file=out_snv)
+						if chrom in outFiles:
+							print("\t".join([sample, chrom, start, ref, mut]), file=outFiles[chrom])
+						else:
+							print(chrom_start + " is not supported. You will need to download that chromosome and create the required files. Continuing with the matrix generation...", file=out)
+							out.flush()
+
+
 
 					# Saves INDEL mutations into an INDEL simple text file
 					else:
@@ -122,7 +157,9 @@ def convertVCF (project, vcf_path, genome, output_path, ncbi_chrom, log_file):
 							out_indel = open(outputFile, "w") 
 							first_indel = False
 
-						print("\t".join([project, sample, ".", genome, "INDEL", chrom, start, start, ref, mut, "SOMATIC"]), file=out_indel)
+						#print("\t".join([project, sample, ".", genome, "INDEL", chrom, start, start, ref, mut, "SOMATIC"]), file=out_indel)
+						print("\t".join([project, sample, ".", genome, "I", chrom, start, start, ref, mut]), file=out_indel)
+
 					prev_line = line
 
 
@@ -130,7 +167,9 @@ def convertVCF (project, vcf_path, genome, output_path, ncbi_chrom, log_file):
 
 	# Closes the output files and returns the boolean flags
 	if snv:
-		out_snv.close()
+		for files in outFiles.values():
+			files.close()
+		#out_snv.close()
 	if indel:
 		out_indel.close()
 	out.close()
