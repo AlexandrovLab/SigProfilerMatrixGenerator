9923ba7146945b988277ca67804f750c64e4e65a
Erik Bergstrom <ebergstr@Eriks-MacBook-Pro.local>
2019-05-08 14:48:40 -0700

    Fixed seqInfo INDEL output issue

--- SigProfilerMatrixGenerator/scripts/SigProfilerMatrixGenerator.py
+++ SigProfilerMatrixGenerator/scripts/SigProfilerMatrixGenerator.py
@@ -1179,7 +1179,7 @@ def catalogue_generator_INDEL_single (mutation_ID, lines, chrom, vcf_path, vcf_p
 						bed_file_tsb.write(sample + '\t' + chrom + '\t' + str(start) + '\t' + indel_key_tsb + "\t" + ref + "\t" + mut + "\n")
 
 					if seqInfo:
-						print("\t".join([sample, chrom, str(start), indel_key_tsb, ref, mut, strand]), file=seqOut)
+						print("\t".join([sample, chrom, str(start), indel_key_tsb, ref, mut, str(strand)]), file=seqOut)
 
 				else:
 					if not functionFlag:
