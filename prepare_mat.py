81fba7d0ca21b6d94adf2b5fa52b620a05694849
Eric T. Dawson <eric.t.dawson@gmail.com>
2019-10-22 15:26:48 -0400

    Add a script that wraps the python calls for preparing the matrix (prepare_mat.py)

---
+++ prepare_mat.py
@@ -0,0 +1,19 @@
+import argparse
+from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--maf", dest="maf", help="MAF file from which to extract matrix.")
+    parser.add_argument("-i", "--indels", action="store_true", dest="indels", help="Extract indel matrix, as well as SBS", default=False)
+    parser.add_argument("-e", "--exome", dest="exome", help="Exome data - restrict genome to exome regions", default=False)
+    parser.add_argument("-p", "--project", dest="project", default="PROJECT", help="Project name for output.")
+    parser.add_argument("-d", "--directory", dest="directory", default="input", help="Input/Output directory")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    
+    args = parse_args()
+
+    matrices = matGen.SigProfilerMatrixGeneratorFunc(args.project, "GRCh37", args.maf, plot=True, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
