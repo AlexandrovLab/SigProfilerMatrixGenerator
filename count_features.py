5f5d0ec80edb15b8718cf9e6263f4e1dec52e331
Eric T. Dawson <eric.t.dawson@gmail.com>
2019-10-22 19:22:06 -0400

    Add script for counting features

---
+++ count_features.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+if __name__ == "__main__":
+
+    with open(sys.argv[1], "r") as ifi:
+        for line in ifi:
+            if not "Mut" in line:
+                tokens = line.strip().split("\t")
+                key = tokens[0]
+                vals = sum([int(i) for i in tokens[1:]])
+                print(key, vals)
