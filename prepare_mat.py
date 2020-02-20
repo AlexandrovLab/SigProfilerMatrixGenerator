439a68bd636f9b4bfdfae13e612a1d893d3c741f
Eric T Dawson <eric.t.dawson@gmail.com>
2020-02-20 19:54:23 +0000

    Add directory creation logic to prepare_mat

--- prepare_mat.py
+++ prepare_mat.py
@@ -1,5 +1,7 @@
 import argparse
-from os.path import dirname
+from os.path import dirname, isdir
+from os import mkdir, getcwd
+import shutil
 from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 
 def parse_args():
@@ -8,8 +10,9 @@ def parse_args():
     parser.add_argument("-i", "--indels", action="store_true", dest="indels", help="Extract indel matrix, as well as SBS", default=False)
     parser.add_argument("-e", "--exome", dest="exome", help="Exome data - restrict genome to exome regions", default=False)
     parser.add_argument("-p", "--project", dest="project", default="PROJECT", help="Project name for output.")
-    parser.add_argument("-d", "--directory", dest="directory", default="input", help="Input/Output directory")
+    parser.add_argument("-d", "--directory", dest="directory", default=None, help="Input/Output directory")
     parser.add_argument("-P", "--plot", dest="plot", action="store_true", help="Output plots of input data.")
+    parser.add_argument("-c", "--cushion", dest="cushion", type=int, default=100, help="Adds an Xbp cushion to the exome/bed_file ranges for downsampling the mutations.")
 
     return parser.parse_args()
 
@@ -17,5 +20,18 @@ def parse_args():
 if __name__ == "__main__":
     
     args = parse_args()
+    
+    if args.directory is None:
+        current = getcwd()
+        args.directory = current + "/" + "sigprof_input"
+    try:
+        if not isdir(args.directory):
+            mkdir(args.directory)
+    except:
+        print("ERROR: creation of directory", args.directory, "failed. Please use the -d option to create a valid directory.")
+    try:
+        shutil.copyfile(args.maf, args.directory + "/" + args.maf)
+    except:
+        print("File copy failed.", args.maf, args.directory + "/" + args.maf)
 
-    matrices = matGen.SigProfilerMatrixGeneratorFunc(args.project, "GRCh37", dirname(args.maf), plot=args.plot, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
+    matrices = matGen.SigProfilerMatrixGeneratorFunc(args.project, "GRCh37", args.directory, plot=args.plot, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=args.cushion)
