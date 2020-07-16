7e4928c152e5e636c540dc227e4718588513af52
Azhar <akhandek@eng.ucsd.edu>
2020-07-15 17:46:03 -0700

    Deleted some files and cleaned up directory structure

--- SigProfilerMatrixGenerator/scripts/temp.py
+++
@@ -1,7 +0,0 @@
-#make sample by feature matrix for nmf with rows as features and samples as columns
-features = []
-with open('./SigProfilerMatrixGenerator/scripts/CNV_features.tsv') as f:
-    for line in f:
-        features.append(line.strip())
-assert(len(features) == 48)
-print(features)
