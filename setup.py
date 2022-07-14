535af54381d6968721ec7e4c46706b60a9fa19f5
mdbarnesUCSD <mdbarnes@ucsd.edu>
2022-07-14 16:35:42 -0700

    Update version to v1.2.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 	# with open('README.rst') as f:
 	# 	return(f.read())
 
-VERSION = '1.2.6'
+VERSION = '1.2.8'
 
 def write_version_py(filename='SigProfilerMatrixGenerator/version.py'):
 	# Copied from numpy setup.py
