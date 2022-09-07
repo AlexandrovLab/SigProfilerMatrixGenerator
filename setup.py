265a6a4c8aa17955be79afeb952073c3c5243c48
mdbarnesUCSD <mdbarnes@ucsd.edu>
2022-09-06 23:02:18 -0700

    Upgrade to v1.2.12

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 	# with open('README.rst') as f:
 	# 	return(f.read())
 
-VERSION = '1.2.11'
+VERSION = '1.2.12'
 
 def write_version_py(filename='SigProfilerMatrixGenerator/version.py'):
 	# Copied from numpy setup.py
