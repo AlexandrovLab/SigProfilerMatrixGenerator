45e5bf11a0b421cb9637592590d9ff7ddfc26cfd
mdbarnesUCSD <mdbarnes@ucsd.edu>
2023-06-06 12:03:23 -0700

    Upgrade to v1.2.17

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import shutil
 
 from setuptools import setup
 
-VERSION = "1.2.16"
+VERSION = "1.2.17"
 
 # remove the dist folder first if exists
 if os.path.exists("dist"):
@@ -23,7 +23,7 @@ def write_version_py(filename="SigProfilerMatrixGenerator/version.py"):
 # THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
-Update = 'v1.2.16: Update MANIFEST and reference dir updates.'
+Update = 'v1.2.17: Skip hidden files, CliController update, pytesting, and organization.'
 
 	"""
     fh = open(filename, "w")
