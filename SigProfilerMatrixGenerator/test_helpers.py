a0dc014eee4237c4e2d0b06cca4a5afbdbd3a9dc
Mousumy Kundu <mousumykundu@Mousumys-MBP.lan>
2024-08-30 15:32:17 -0700

    added test

--- SigProfilerMatrixGenerator/test_helpers.py
+++ SigProfilerMatrixGenerator/test_helpers.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
@@ -10,7 +9,8 @@ from SigProfilerMatrixGenerator.scripts import (
 )
 
 reference_dir = ref_install.reference_dir()
-TEST_INPUT_DIR = str(reference_dir.path / "references/tests/")
+TEST_INPUT_DIR = str(reference_dir.path / "references/tests/") + "/"
+BED_FILE_DIR = str(reference_dir.path / "references/chromosomes/exome") + "/"
 FILE_PREF = "test_example"
 TEST_GENOMES = [
     "c_elegans",
@@ -25,32 +25,87 @@ TEST_GENOMES = [
 ]
 
 
-def load_and_compare(matrices, solution_dir):
-    # loop through every key in matrices and load the corresponding solution file
+def load_and_compare(matrices, solution_dir, exome=False, bed_file=True):
     for key in matrices:
-        solution_file = os.path.join(solution_dir, FILE_PREF + ".SBS" + key + ".all")
-        solution_df = pd.read_csv(solution_file, sep="\t", index_col=0)
-        # check that the dataframes are equal
-        assert_frame_equal(matrices[key], solution_df)
+        # Only process keys that start with "SBS"
+        if not key.startswith("SBS"):
+            continue  # Skip non-SBS matrices
+
+        # Determine the solution file path based on the exome flag
+        if exome:
+            solution_file = os.path.join(
+                solution_dir, FILE_PREF + ".SBS" + key + ".exome"
+            )
+        elif bed_file:
+            solution_file = os.path.join(
+                solution_dir, FILE_PREF + ".SBS" + key + ".region"
+            )
+        else:
+            solution_file = os.path.join(
+                solution_dir, FILE_PREF + ".SBS" + key + ".all"
+            )
+
+        # Load the solution file and compare with the generated matrix
+        if os.path.exists(solution_file):
+            solution_df = pd.read_csv(solution_file, sep="\t", index_col=0)
+            assert_frame_equal(matrices[key], solution_df)
 
 
-def test_one_genome(genome, volume):
-    matrices = matGen.SigProfilerMatrixGeneratorFunc(
-        FILE_PREF,
-        genome,
-        os.path.join(TEST_INPUT_DIR, genome),
-        plot=False,
-        exome=False,
-        bed_file=None,
-        chrom_based=False,
-        tsb_stat=False,
-        seqInfo=False,
-        cushion=100,
-        volume=volume,
-    )
+def test_one_genome(genome, volume, exome=False, bed_file=True):
+    if exome:
+        matrices = matGen.SigProfilerMatrixGeneratorFunc(
+            FILE_PREF,
+            genome,
+            os.path.join(TEST_INPUT_DIR + "WES", genome),
+            plot=False,
+            exome=True,
+            bed_file=None,
+            chrom_based=False,
+            tsb_stat=False,
+            seqInfo=False,
+            cushion=100,
+            volume=volume,
+        )
+    elif bed_file:
+        matrices = matGen.SigProfilerMatrixGeneratorFunc(
+            FILE_PREF,
+            genome,
+            os.path.join(TEST_INPUT_DIR + "bed_file", genome),
+            plot=False,
+            exome=False,
+            bed_file=os.path.join(
+                BED_FILE_DIR + genome + "/" + genome + "_exome.interval_list"
+            ),
+            chrom_based=False,
+            tsb_stat=False,
+            seqInfo=False,
+            cushion=100,
+            volume=volume,
+        )
+
+    else:
+        matrices = matGen.SigProfilerMatrixGeneratorFunc(
+            FILE_PREF,
+            genome,
+            os.path.join(TEST_INPUT_DIR + "WGS", genome),
+            plot=False,
+            exome=False,
+            bed_file=None,
+            chrom_based=False,
+            tsb_stat=False,
+            seqInfo=False,
+            cushion=100,
+            volume=volume,
+        )
 
     # load the results from the solution directory and compare them to the dataframes in matrices
-    solution_dir = os.path.join(TEST_INPUT_DIR, "solutions/" + genome + "/")
+    if exome:
+        solution_dir = os.path.join(TEST_INPUT_DIR, f"WES/solutions/{genome}/")
+        print("solution_dir", solution_dir)
+    elif bed_file:
+        solution_dir = os.path.join(TEST_INPUT_DIR, f"bed_file/solutions/{genome}/")
+    else:
+        solution_dir = os.path.join(TEST_INPUT_DIR, f"WGS/solutions/{genome}/")
     load_and_compare(matrices, solution_dir)
 
 
@@ -93,3 +148,16 @@ def test_genomes(test_genome, volume=None):
                 print("Completed test for " + genome)
             except Exception as e:
                 assert False, "Test failed for " + genome + ":\n" + str(e)
+
+
+def test_all_genomes(volume=None):
+    for genome in TEST_GENOMES:
+        try:
+            test_one_genome(genome, volume)
+            print(f"Completed test for {genome}\n")
+        except Exception as e:
+            print(f"Test failed for {genome}:\n{str(e)}\n")
+
+
+# Run the tests for all genomes
+test_all_genomes()
